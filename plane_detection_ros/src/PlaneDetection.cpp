646b385117eec14d081cb217fb3fda90a37d0636
Lovro Markovic <lovro.markovic@fer.hr>
2019-04-08 14:30:56 +0200

    [plane-detection] Include and use pcl library

--- plane_detection_ros/src/PlaneDetection.cpp
+++ plane_detection_ros/src/PlaneDetection.cpp
@@ -5,7 +5,14 @@
  *      Author: Lovro Markovic
  */
 
+// PCL includes
+#include <pcl-1.8/pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+
+// ROS includes
 #include <ros/console.h>
+
+// Own includes
 #include "PlaneDetection.h"
 
 PlaneDetection::PlaneDetection()
@@ -16,11 +23,8 @@ PlaneDetection::~PlaneDetection()
 {
 }
 
-
-
 void PlaneDetection::pointCloudCallback(
 		const sensor_msgs::PointCloud2ConstPtr& pointCloud)
 {
-	ROS_INFO("Hello from callback");
 }
 
