0b9363a349942ba55bb5505c88d0f4e3876b43a1
RobertMilijas <robert.milijas@fer.hr>
2019-09-23 00:49:57 +0200

    First working example of visual servoing.
    Needs tuning and implementation of yaw setpoint.

--- uav_ros_control/include/uav_ros_control/reference/VisualServo.h
+++ uav_ros_control/include/uav_ros_control/reference/VisualServo.h
@@ -11,9 +11,9 @@
 #include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/PoseWithCovariance.h>
-#include <tf/transform_listener.h>
+#include <tf2/LinearMath/Quaternion.h>
 
-
+#include <ros/ros.h>
 #include <ros/publisher.h>
 #include <ros/subscriber.h>
 #include <ros/service_server.h>
@@ -25,68 +25,65 @@ namespace uav_reference {
   * The reference is published in the global coordinate system.
   **/
   class VisualServo {
-      public:
-        /**
-         * Default constructor.
-         */
-        VisualServo(ros::NodeHandle&);
-        virtual ~VisualServo();
+   public:
+     /**
+      * Default constructor.
+      */
+     VisualServo(ros::NodeHandle&);
+     virtual ~VisualServo();
 
-        /**
-         * Update position setpoint.
-         */
-         void updateSetpoint();
+     /**
+      * Update position setpoint.
+      */
+     void updateSetpoint();
 
-         /**
-          * Publish new setpoint as MultiDOFJointTrajectoryPoint
-          */
-          void publishNewSetpoint();
+     /**
+      * Publish new setpoint as MultiDOFJointTrajectoryPoint
+      */
+     void publishNewSetpoint();
 
-          // fali updateCarrotStatus();
-
-          bool isVisualServoEnabled();
+     bool isVisualServoEnabled();
 
    private:
-
-      void intializeParameters();
 
       /**
        * Callback function for StartVisualServo service
        */
-       bool startVisualServoServiceCb(std_srvs::Empty::Request& request,
-           std_srvs::Empty::Response& response);
+      bool startVisualServoServiceCb(std_srvs::Empty::Request& request,
+          std_srvs::Empty::Response& response);
 
-        /**
-         * Odometry callback function for extracting the UAV's pose.
-         */
-         void odomCb(const nav_msgs::OdometryConstPtr&);
+      /**
+        * Odometry callback function for extracting the UAV's pose.
+        */
+      void odomCb(const nav_msgs::OdometryConstPtr&);
 
-         void pitchErrorCb(const std_msgs::Float32);
-         void yawErrorCb(const std_msgs::Float32);
+      /**
+       * Callback functions for the visual servo process values.
+       */
+      void pitchErrorCb(const std_msgs::Float32&);
+      void yawErrorCb(const std_msgs::Float32&);
 
-         std::array<double, 3> _uavPos{0.0, 0.0, 0.0};
-         std::array<double, 3> _setpointPosition{0.0, 0.0, 0.0};
-         double _dx, _dy, _uav_yaw;
+      std::array<double, 3> _uavPos{0.0, 0.0, 0.0};
+      std::array<double, 3> _setpointPosition{0.0, 0.0, 0.0};
+      double _dx, _dy, _uavYaw, _setpointYaw;
 
-         //bool _positionHold = false;
-         bool _visualServoEnabled = false;
+      //bool _positionHold = false;
+      bool _visualServoEnabled = false;
 
-         /** Publishers */
-         ros::Publisher _pubNewSetpoint;
+      /** Publishers */
+      ros::Publisher _pubNewSetpoint;
+      trajectory_msgs::MultiDOFJointTrajectoryPoint _new_point;
 
-         /** Subscribers */
-         ros::Subscriber _subOdom;
-         ros::Subscriber _subYawError, _subPitchError; // As published by erl_husky color_filter node.
+      /** Subscribers */
+      ros::Subscriber _subOdom;
+      ros::Subscriber _subYawError, _subPitchError; // As published by erl_husky color_filter node.
 
-         /** Services */
-         ros::ServiceServer _serviceStartVisualServo;
-
-         /** TF **/
-         tf::TransformListener tf_listener;
+      /** Services */
+      ros::ServiceServer _serviceStartVisualServo;
 
   };
+
+  void runDefault(VisualServo& cc, ros::NodeHandle& nh);
 }
-
-
 
 #endif //UAV_ROS_CONTROL_VISUALSERVO_H
--- uav_ros_control/nodes/reference/VisualServoNode.cpp
+++ uav_ros_control/nodes/reference/VisualServoNode.cpp
@@ -5,6 +5,6 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "visual_servo_node");
     ros::NodeHandle nh;
 
-    std::shared_ptr<uav_reference:VisualServo> visualServoObj{new uav_reference::VisualServo(nh)};
-    uav::reference::runDefault(*visualServoObj, nh);
+    std::shared_ptr<uav_reference::VisualServo> visualServoObj{new uav_reference::VisualServo(nh)};
+    uav_reference::runDefault(*visualServoObj, nh);
 }
