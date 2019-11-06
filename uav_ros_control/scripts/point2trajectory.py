47e5db382961aaf74c2b0a070a0836b9ce9be2d2
Lovro Markovic <lovro.markovic@fer.hr>
2019-11-06 10:51:59 +0100

    Point2Trajectory publishes odometry references while carrot is disabled

--- uav_ros_control/scripts/point2trajectory.py
+++ uav_ros_control/scripts/point2trajectory.py
@@ -2,6 +2,9 @@
 
 import rospy
 from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Transform, Quaternion
 
 class TrajectoryPacker():
     '''
@@ -10,13 +13,42 @@ class TrajectoryPacker():
 
     def __init__(self):
         rospy.Subscriber('trajectory_point', MultiDOFJointTrajectoryPoint, self.ref_sub)
+        rospy.Subscriber('odometry', Odometry, self.odom_sub)
+        self.odom_msg = Odometry()
+        rospy.Subscriber('carrot/status', String, self.status_cb)
+        self.pub_odom = True
         self.traj_pub = rospy.Publisher('trajectory', MultiDOFJointTrajectory, queue_size=1)
 
     def ref_sub(self, msg):
         traj = MultiDOFJointTrajectory()
         traj.points = []
-        traj.points.append(msg)
+
+        if not self.pub_odom:
+            traj.points.append(msg)
+        else:
+            # Take it from odometry
+            point = MultiDOFJointTrajectoryPoint()
+            transform = Transform()
+            transform.translation.x = self.odom_msg.pose.pose.position.x
+            transform.translation.y = self.odom_msg.pose.pose.position.y
+            transform.translation.z = self.odom_msg.pose.pose.position.z
+            transform.rotation.x = self.odom_msg.pose.pose.orientation.x
+            transform.rotation.y = self.odom_msg.pose.pose.orientation.y
+            transform.rotation.z = self.odom_msg.pose.pose.orientation.z
+            transform.rotation.w = self.odom_msg.pose.pose.orientation.w
+            point.transforms.append(transform)
+            traj.points.append(point)
+
         self.traj_pub.publish(traj)
+        
+    def status_cb(self, msg):
+        if msg.data == "OFF":
+            self.pub_odom = True
+        else:
+            self.pub_odom = False
+
+    def odom_sub(self, msg):
+        self.odom_msg = msg
 
 if __name__ == "__main__":
     rospy.init_node('point_to_trajectory')
