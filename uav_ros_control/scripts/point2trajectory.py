89b2da1f839af9ee987bc38d6742b981903df97c
Lovro Markovic <lovro.markovic@fer.hr>
2019-11-13 12:57:30 +0100

    Fix point2trajectory script

--- uav_ros_control/scripts/point2trajectory.py
+++ uav_ros_control/scripts/point2trajectory.py
@@ -4,7 +4,8 @@ import rospy
 from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
 from std_msgs.msg import String
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Transform, Quaternion
+from geometry_msgs.msg import Transform, Quaternion, Vector3Stamped
+from math import atan2, cos, sin
 
 class TrajectoryPacker():
     '''
@@ -17,8 +18,10 @@ class TrajectoryPacker():
         self.odom_msg = Odometry()
         rospy.Subscriber('carrot/status', String, self.status_cb)
         self.pub_odom = True
-        self.status = "OFF"
+        self.old_odom = Odometry()
+	self.status = "OFF"
         self.traj_pub = rospy.Publisher('trajectory', MultiDOFJointTrajectory, queue_size=1)
+	self.dummy_pub = rospy.Publisher('dummy/velocity', Vector3Stamped, queue_size=1)
 
     def ref_sub(self, msg):
         traj = MultiDOFJointTrajectory()
@@ -44,9 +47,26 @@ class TrajectoryPacker():
             transform.rotation.w = self.odom_msg.pose.pose.orientation.w
             point.transforms.append(transform)
             traj.points.append(point)
+	    self.traj_pub.publish(traj)
 
     def odom_sub(self, msg):
         self.odom_msg = msg
+	
+	dx = msg.pose.pose.position.x - self.old_odom.pose.pose.position.x
+	dy = msg.pose.pose.position.y - self.old_odom.pose.pose.position.y
+	dz = msg.pose.pose.position.z - self.old_odom.pose.pose.position.z
+	qx = msg.pose.pose.orientation.x
+	qy = msg.pose.pose.orientation.y
+	qz = msg.pose.pose.orientation.z
+	qw = msg.pose.pose.orientation.w
+	yaw = atan2(2 * (qw * qz + qx * qy), qw * qw + qx * qx - qy * qy - qz * qz)
+	newMsg = Vector3Stamped()
+	newMsg.vector.x = (cos(yaw) * dx + sin(yaw) * dy) / 0.02
+	newMsg.vector.y = (cos(yaw) * dy - sin(yaw) * dx ) / 0.02
+	newMsg.vector.z = yaw
+	newMsg.header.stamp = rospy.Time.now()
+	self.dummy_pub.publish(newMsg)
+	self.old_odom = msg
 
 if __name__ == "__main__":
     rospy.init_node('point_to_trajectory')
