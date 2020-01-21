db10247b284885c3b51198893ee36fc0beb2508e
Lovro Markovic <lovro.markovic@fer.hr>
2020-01-21 13:43:35 +0100

    [BrickPickupStateMachine] Add service callers for color_filter and brick_pickup/local initailzation

--- uav_ros_control/include/uav_ros_control/reference/BrickPickupStateMachine.h
+++ uav_ros_control/include/uav_ros_control/reference/BrickPickupStateMachine.h
@@ -8,6 +8,11 @@
 #include <uav_ros_control/filters/Util.h>
 #include <iostream>
 #include <uav_ros_control/reference/Global2Local.h>
+#include <uav_ros_control/reference/TrajectoryGenerator.h>
+#include <nav_msgs/Odometry.h>
+#include <std_srvs/SetBool.h>
+#include <color_filter/color.h>
+
 using namespace ros_util;
 
 namespace uav_sm {
@@ -25,7 +30,7 @@ enum class VisualServoState {
 };
 
 struct BrickPickupStatus {
-  BrickPickupStatus() : BrickPickupStatus("default", Eigen::Vector3d(0, 0, 0)) { }
+  BrickPickupStatus() : BrickPickupStatus("red", Eigen::Vector3d(0, 0, 0)) { }
   BrickPickupStatus(const std::string& t_brickColor, const Eigen::Vector3d& t_brickLocal, 
     const BrickPickupStates t_status = BrickPickupStates::OFF) :
       m_brickColor(t_brickColor),
@@ -47,9 +52,17 @@ class BrickPickupStateMachine {
 public:
 BrickPickupStateMachine(ros::NodeHandle& t_nh) :
     m_handlerVSSMState(t_nh, "visual_servo_sm/state"),
+    m_handlerOdometry(t_nh, "odometry"),
     m_global2Local(t_nh) {
-  initializeParameters(t_nh);
+  initialize_parameters(t_nh);
   
+  // Initialize service callers
+  m_vssmCaller = t_nh.serviceClient
+    <std_srvs::SetBool::Request, std_srvs::SetBool::Response>("brick_pickup/local");
+  m_chooseColorCaller = t_nh.serviceClient
+    <color_filter::color::Request, color_filter::color::Response>("filter_color");
+
+  // Advertise service
   m_serviceBrickPickup = t_nh.advertiseService(
     "brick_pickup/global",
     &uav_sm::BrickPickupStateMachine::brick_pickup_global_cb,
@@ -90,17 +103,47 @@ inline const VisualServoState getCurrentVisualServoState() {
   return static_cast<VisualServoState>(m_handlerVSSMState.getData().data);
 }
 
-void initializeParameters(ros::NodeHandle& nh) {
+void initialize_parameters(ros::NodeHandle& nh) {
   PickupParams initParams;
   initParams.dummy_param = getParamOrThrow<double>(nh, "brick_pickup/dummy_param");
   m_pickupConfig.reset(new ParamHandler<PickupParams>(initParams, "brick_pickup"));
 }
 
+bool toggle_visual_servo_state_machine(bool t_enable = true) {
+  std_srvs::SetBool::Request req;
+  std_srvs::SetBool::Response resp;
+  req.data = t_enable;
+
+  if (!m_vssmCaller.call(req, resp)) {
+    ROS_FATAL("BrickPickup - unable to activate VSSM");
+    return false;
+  }
+
+  // At this point VSSM is successfully toggled, so return true
+  return true;
+}
+
+bool filter_choose_color(const std::string& t_color) {
+  color_filter::color::Request req;
+  color_filter::color::Response resp;
+  req.color = t_color;
+
+  if (!m_chooseColorCaller.call(req, resp)) {
+    ROS_FATAL("BrickPickup - color initialization failed");
+    return false;
+  }
+
+  // At this point color_initialization is assumed to be finished
+  return true;
+}
+
+ros::Timer m_runTimer;
 Global2Local m_global2Local;
 BrickPickupStatus m_currentStatus;
 ros::ServiceServer m_serviceBrickPickup;
-ros::Timer m_runTimer;
+ros::ServiceClient m_vssmCaller, m_chooseColorCaller;
 TopicHandler<std_msgs::Int32> m_handlerVSSMState;
+TopicHandler<nav_msgs::Odometry> m_handlerOdometry;
 std::unique_ptr<ParamHandler<PickupParams>> m_pickupConfig;
 };
 }
