3b195bc5ee3b836a31bbdb9d02b5b23ac677e987
Lovro Markovic <lovro.markovic@fer.hr>
2020-02-09 22:12:39 +0100

    [MasterPickup] Start implementation for master pickup

---
+++ uav_ros_control/include/uav_ros_control/reference/MasterPickupControl.h
@@ -0,0 +1,27 @@
+#ifndef MASTER_PICKUP_CONTROL_H
+#define MASTER_PICKUP_CONTROL_H
+
+#include <ros/ros.h>
+#include <uav_ros_control/filters/Util.h>
+#include <uav_ros_control/reference/PickupStates.h>
+
+using namespace ros_util;
+namespace uav_sm 
+{
+
+class MasterPickupControl
+{
+public:
+
+MasterPickupControl()
+{
+
+}
+
+private:
+
+};
+
+}
+
+#endif /* MASTER_PICKUP_CONTROL_H */
