f701db1b190e2e904afcc7f99dcdf513a0e8fd11
Lovro Markovic <lovro.markovic@fer.hr>
2020-02-14 15:23:11 +0100

    [magnet_override_sim.py] Properly do the ros Service in python

--- uav_ros_control/scripts/magnet_override_sim.py
+++ uav_ros_control/scripts/magnet_override_sim.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from std_srvs.srv import Empty
+from std_srvs.srv import Empty, EmptyResponse
 from std_msgs.msg import Float32
 
 class MagnetOverride:
@@ -16,12 +16,14 @@ class MagnetOverride:
     offMsg = Float32()
     offMsg.data = 1.0
     self.magnet_pub.publish(offMsg)
+    return EmptyResponse()
   
   def override_off_cb(self, req):
     print("MagnetOverride - turning OFF magnet")
     offMsg = Float32()
     offMsg.data = 0.0
     self.magnet_pub.publish(offMsg)
+    return EmptyResponse()
 
 if __name__ == "__main__":
   rospy.init_node("magnet_override_node")
