09a1bb139cb404209f66a3260213ca518c22e7e9
Lovro Markovic <lovro.markovic@fer.hr>
2020-10-22 17:32:09 +0200

    Add Cvxgen MPC C files from https://github.com/ctu-mrs/mrs_uav_trackers

---
+++ uav_ros_tracker/README.md
@@ -0,0 +1,3 @@
+# UAV ROS Tracker
+
+This repository contains MPC-based UAV tracking algorithm. Inspired by [ctu-mrs/mrs_uav_trackers](https://github.com/ctu-mrs/mrs_uav_trackers).
---
+++ uav_ros_tracker/include/uav_ros_tracker/cvx_wrapper.h
@@ -0,0 +1,39 @@
+#ifndef CVX_WRAPPER_TRACKER
+#define CVX_WRAPPER_TRACKER
+
+#include <ros/ros.h>
+#include <eigen3/Eigen/Eigen>
+
+namespace uav_ros_trackers
+{
+
+namespace cvx_wrapper
+{
+
+/* author: Daniel Hert */
+
+class CvxWrapper {
+
+public:
+  CvxWrapper(bool verbose, int max_iters, std::vector<double> tempQ, double dt, double dt2, int dim);
+
+  void   setInitialState(Eigen::MatrixXd &x);
+  bool   setVelQ(double Q_vel);
+  bool   setQ(std::vector<double> Qnew);
+  void   loadReference(Eigen::MatrixXd &reference);
+  void   setLimits(double max_speed, double min_speed, double max_acc, double min_acc, double max_jerk, double min_jerk, double max_snap, double min_snap);
+  int    solveCvx();
+  void   getStates(Eigen::MatrixXd &future_traj);
+  double getFirstControlInput();
+
+private:
+  static const int    horizon_len = 40;
+  std::vector<double> myQ;
+  int                 dim;
+};
+
+}  // namespace cvx_wrapper
+
+}  // namespace uav_ros_trackers
+
+#endif
